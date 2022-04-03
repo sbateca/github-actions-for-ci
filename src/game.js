bec0f3f659904e2e8fd90a946e777f7560135817
Sergio Bateca <sbtk87@hotmail.com>
2022-04-03 15:45:19 -0500

    Add standard --fix into node.js.yml

--- src/game.js
+++ src/game.js
@@ -10,21 +10,17 @@ export default class Game {
         this.player = Math.random() < 0.5 ? this.p1 : this.p2
         this.sym = 'X'
     }
-
     turn(row, col) {
         col = col || row
         this.board[row][col] = this.sym
     }
-
     nextPlayer() {
         this.player = this.player === this.p1 ? this.p2 : this.p1
         this.sym = this.sym === 'X' ? 'O' : 'X'
     }
-
     hasWinner() {
         return this.rowWin() || this.colWin() || this.diagWin()
     }
-
     rowWin() {
         let win = false
         for (let r = 0; r < 3; r++) {
@@ -32,10 +28,8 @@ export default class Game {
             if (row[0] === null) { continue }
             win = win || (row[0] === row[1] && row[0] === row[2])
         }
-
         return win
     }
-
     colWin() {
         let win = false
         for (let c = 0; c < 3; c++) {
@@ -43,10 +37,8 @@ export default class Game {
             if (col[0][c] === null) { continue }
             win = win || (col[0][c] === col[1][c] && col[0][c] === col[2][c])
         }
-
         return win
     }
-
     diagWin() {
         const b = this.board
         return ((b[0][0] !== null && b[0][0] === b[1][1] && b[0][0] === b[2][2]) ||
